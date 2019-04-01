eeb9ab20b5b4668f4835da5d613b1ce8cdf92b43
rpkr <13591644403@139.com>
2019-04-01 14:45:33 +0800

    creat txt内容检测

---
+++ 查询一个txt中的内容在另一个txt中有多少/查询一个字符串是否在另一个文件中.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+def str_search(target_txt, assets_txt):
+    # 第一个参数危要查寻的txt，第二个为被查询的txt
+    # 获取要查找的字符串
+    targets = []
+    f = open(target_txt, "r")
+    lines = f.readlines()
+    for line in lines:
+        targets.append(line.strip())
+
+    # 获取被查找的字符串
+    assets = []
+    f1 = open(assets_txt, "r")
+    lines1 = f1.readlines()
+    for line in lines1:
+        assets.append(line.strip())
+
+    for target in targets:
+        if target in assets:
+            print(target)
+
+
+if __name__ == "__main__":
+    str_search("target.txt", "assets.txt")
