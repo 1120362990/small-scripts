07e5c2c64b4e8f446b88c0a38fc4af553740cac6
rpkr <13591644403@139.com>
2019-11-25 10:39:34 +0800

    fix:修复了一个愚蠢的错误

--- nmap-xml-整理/nmap-xml-整理.py
+++ nmap-xml-整理/nmap-xml-整理.py
@@ -10,22 +10,31 @@ def nmap_xls_res(xml):
 
     soup = BeautifulSoup(open(xml),features="lxml")
     for num in range(0,len(soup.find_all('host'))):
-        if soup.find_all('host')[num].find_all('status')[0]['state'] == 'down':
-            pass
+        # print(str(soup.find_all('host')[num].find_all('address')[0]['addr']))  # ip
+        # print(soup.find_all('host')[num].find_all('status')[0]['state']) # ip state
+        if soup.find_all('host')[num].find_all('status')[0]['state'] == 'up':
+            # print(str(soup.find_all('host')[num].find_all('address')[0]['addr']))#ip
+            for num2 in range(0,len(soup.find_all('host')[num].find_all('port'))):
+                if str(soup.find_all('host')[num].find_all('state')[num2]['state']) == 'open':
+                # print(str(soup.find_all('host')[num].find_all('state')[num2]['state']))#port stat
+                    # print(str(soup.find_all('host')[num].find_all('address')[0]['addr'])+'	'+str(soup.find_all('host')[num].find_all('port')[num2]['portid'])+'	'+'1')#port
+                    try:
+                        print(str(soup.find_all('host')[num].find_all('address')[0]['addr'])+'	'+str(soup.find_all('host')[num].find_all('port')[num2]['portid'])+'	'+soup.find_all('host')[num].find_all('service')[num2]['name'])#ip port service
+                        worksheet.write(n,0, label = str(soup.find_all('host')[num].find_all('address')[0]['addr']))
+                        worksheet.write(n,1, label = str(soup.find_all('host')[num].find_all('port')[num2]['portid']))
+                        worksheet.write(n,2, label = str(soup.find_all('host')[num].find_all('service')[num2]['name']))
+                        n = n+1
+                    except Exception:
+                        print(str(soup.find_all('host')[num].find_all('address')[0]['addr'])+'	'+str(soup.find_all('host')[num].find_all('port')[num2]['portid']))#ip port    ,some ip  don't have service name
+                        worksheet.write(n,0, label = str(soup.find_all('host')[num].find_all('address')[0]['addr']))
+                        worksheet.write(n,1, label = str(soup.find_all('host')[num].find_all('port')[num2]['portid']))
+                        n = n+1
+                else:
+                    pass
         else:
-            for num2 in range (0,len(soup.find_all('host')[num].find_all('port'))):
-                print(str(soup.find_all('host')[num].find_all('address')[0]['addr'])+':'+str(soup.find_all('host')[num].find_all('port')[num2]['portid']),soup.find_all('host')[num].find_all('service')[num2]['name'])
-                worksheet.write(n,0, label = str(soup.find_all('host')[num].find_all('address')[0]['addr'])+':'+str(soup.find_all('host')[num].find_all('port')[num2]['portid']))
-                worksheet.write(n,1, label = str(soup.find_all('host')[num].find_all('service')[num2]['name']))
-                n = n + 1
+            pass
     workbook.save('result.xls')
 
 
 if __name__ == "__main__":
-    nmap_xls_res(r'F:\OneDrive\梦工厂\nmap-xml-整理\1.xml')
-
-
-
-
-
-
+    nmap_xls_res(r'F:\1.xml')
